8ed46544f4db61c23385ae88a5ac19e33d80bfa8
louismerlin <louis.merlin@epfl.ch>
2016-10-26 16:32:38 +0200

    Set speed to 60fps

--- www/js/main.js
+++ www/js/main.js
@@ -4,7 +4,7 @@ var enemy = new Vue({
     barColor: '#1c448e',
     name: 'Blorg the destroyer',
     maxHp: 100,
-    hp: 90
+    hp: 10
   },
   computed: {
     getWidth: function() {
@@ -12,3 +12,11 @@ var enemy = new Vue({
     }
   }
 })
+
+
+var loop = function() {
+  enemy.hp = enemy.hp - 1
+  if (enemy.hp <= 0) enemy.hp = enemy.maxHp
+}
+
+window.setInterval(function() { loop() }, 1000/60)
